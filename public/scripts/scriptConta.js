cb98077148eefa53d964f82f2490f66900af8a72
Th-Sanchez <thiago.peluci@sptech.shool>
2024-11-29 21:22:51 -0300

    Estilização da pagina de troca de nome

--- public/scripts/scriptConta.js
+++ public/scripts/scriptConta.js
@@ -1,104 +1,3 @@
-function limparSessao() {
-    sessionStorage.clear();
-    window.location = "./index.html";
-}
-
-function excluirConta() {
-    var idUsuarioVar = sessionStorage.ID_USUARIO;
-    var nomeVar = sessionStorage.NOME_USUARIO;
-    var emailVar = sessionStorage.EMAIL_USUARIO;
-    var senhaVar = sessionStorage.SENHA_USUARIO;
-
-    var nome = input_nome_del.value;
-    var email = input_email_del.value;
-    var senha = input_senha_del.value;
-
-    if (nome != nomeVar) {
-        return alert('Nome incorreto!')
-    } else if (email != emailVar) {
-        return alert('Email incorreto!')
-    } else if (senha != senhaVar) {
-        return alert('Senha incorreta!')
-    }
-
-    fetch("/usuarios/excluirConta", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            idUsuarioServer: idUsuarioVar
-        })
-    }).then(function (resposta) {
-        console.log("ESTOU NO THEN DO entrar()!")
-
-        if (resposta.ok) {
-            console.log(resposta);
-            resposta.json().then(json => {
-                console.log(json);
-
-                sessionStorage.clear()
-
-                setTimeout(function () {
-                    window.location = "/index.html";
-                }, 1000); // apenas para exibir o loading
-            });
-
-        } else {
-
-            console.log("Houve um erro ao tentar excluir a conta!");
-            resposta.text().then(texto => {
-                console.error(texto);
-            });
-        }
-
-    }).catch(function (erro) {
-        console.log(erro);
-    })
-
-    return false;
-}
-
-function trocarNome() {
-    var idUsuarioVar = sessionStorage.ID_USUARIO;
-    var nomeVar = input_nome.value
-
-    fetch("/usuarios/trocarNome", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            idUsuarioServer: idUsuarioVar
-        })
-    }).then(function (resposta) {
-        console.log("ESTOU NO THEN DO entrar()!")
-
-        if (resposta.ok) {
-            console.log(resposta);
-            resposta.json().then(json => {
-                console.log(json);
-
-                setTimeout(function () {
-                    window.location = "/index.html";
-                }, 1000); // apenas para exibir o loading
-            });
-
-        } else {
-
-            console.log("Houve um erro ao tentar excluir a conta!");
-            resposta.text().then(texto => {
-                console.error(texto);
-            });
-        }
-
-    }).catch(function (erro) {
-        console.log(erro);
-    })
-
-    return false;
-}
-
 function trocarTela(telaAtual) {
     var dashboard = document.getElementById('dashboard')
     var nome = document.getElementById('nome')
@@ -225,18 +124,12 @@ function numeroPostsMes() {
                 const grafico_post = document.getElementById('myChart')
                 grafico_post.style.backgroundColor = '#0A0A0A'  
 
-                var listaNumeroPosts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-                for (var i = 0; i < json.length; i++) {
-                    listaNumeroPosts.splice(json[i].mes - 1, 1, json[i].qtdPosts);
-                }
-
                 const labels = [`Janeiro`, `Fevereiro`, `Março`, `Abril`, `Maio`, `Junho`, `Julho`, `Agosto`, `Setembro`, `Outubro`, `Novembro`, `Dezembro`];
                 const data = {
                     labels: labels,
                     datasets: [{
                         label: '',
-                        data: listaNumeroPosts,
+                        data: [],
                         backgroundColor: [
                             '#F7B61C'
                         ],
@@ -274,6 +167,16 @@ function numeroPostsMes() {
                     },
                 };
 
+                for (var i = 0; i < json.length; i++) {
+                    if (json[i].mes == i+1) {
+                        if (json[i].qtdPosts != 0) {
+                            data.datasets[0].data.push(json[i].qtdPosts)
+                        } else {
+                            data.datasets[0].data.push(0)
+                        }
+                    }
+                }
+
                 const graficoPost = new Chart(grafico_post, config);
             });
         } else {
@@ -285,4 +188,126 @@ function numeroPostsMes() {
     }).catch(erro => {
         console.log(erro);
     })
+}
+
+function confirmarTrocarNome() {
+    const nomeUsuario = JSON.parse(sessionStorage.NOME_USUARIO);
+    const nomeAtual = input_nome_atual.value
+    const novoNome = input_novo_nome.value
+    const confirmarNovoNome = input_cnovo_nome.value
+
+    if (nomeAtual != nomeUsuario) {
+        return alert('O nome atual está incorreto!')
+    } else if (nomeAtual == novoNome) {
+        return alert('O novo nome é igual ao nome antigo!')
+    } else if (novoNome != confirmarNovoNome) {
+        return alert('A confirmarção de nome está errada!')
+    }
+
+    const displayConfirmarcao = document.getElementById('confirmarNome');
+    const sectionConfirmacoes = document.getElementById('confirmacoes');
+
+    displayConfirmarcao.style.display = 'flex';
+    sectionConfirmacoes.style.display = 'flex';
+}
+
+function excluirConta() {
+    var idUsuarioVar = sessionStorage.ID_USUARIO;
+    var nomeVar = sessionStorage.NOME_USUARIO;
+    var emailVar = sessionStorage.EMAIL_USUARIO;
+    var senhaVar = sessionStorage.SENHA_USUARIO;
+
+    var nome = input_nome_del.value;
+    var email = input_email_del.value;
+    var senha = input_senha_del.value;
+
+    if (nome != nomeVar) {
+        return alert('Nome incorreto!')
+    } else if (email != emailVar) {
+        return alert('Email incorreto!')
+    } else if (senha != senhaVar) {
+        return alert('Senha incorreta!')
+    }
+
+    fetch("/usuarios/excluirConta", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            idUsuarioServer: idUsuarioVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+            resposta.json().then(json => {
+                console.log(json);
+
+                sessionStorage.clear()
+
+                setTimeout(function () {
+                    window.location = "/index.html";
+                }, 1000); // apenas para exibir o loading
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar excluir a conta!");
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
+
+function trocarNome() {
+    var idUsuarioVar = sessionStorage.ID_USUARIO;
+    var nomeVar = input_nome.value
+
+    fetch("/usuarios/trocarNome", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            idUsuarioServer: idUsuarioVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+            resposta.json().then(json => {
+                console.log(json);
+
+                setTimeout(function () {
+                    window.location = "/index.html";
+                }, 1000); // apenas para exibir o loading
+            });
+
+        } else {
+
+            console.log("Houve um erro ao tentar excluir a conta!");
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "./index.html";
 }
